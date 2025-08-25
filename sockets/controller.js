47bb23cc053603e823c7aba39d517f292983d37e
jafetsolano <jafet.solano@vml.com>
2025-08-24 20:17:18 -0600

    socket added

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,18 +1,16 @@
+const TicketControl = require("../models/ticket-control");
 
-
+const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
     
-    socket.on('enviar-mensaje', ( payload, callback ) => {
-        
-        const id = 123456789;
-        callback( id );
-
-        socket.broadcast.emit('enviar-mensaje', payload );
-
+    socket.on('sigiente-ticket', ( payload, callback ) => {
+        const siguiente = ticketControl.siguiente();
+        callback( siguiente );
     })
 
 }
+
 
 
 
