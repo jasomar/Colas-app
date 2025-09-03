9fde9f60897e638fa283e9f2a131f5c1eaefe1c1
jafetsolano <jafet.solano@vml.com>
2025-09-02 21:20:36 -0600

    socket communication addded

--- sockets/controller.js
+++ sockets/controller.js
@@ -3,6 +3,8 @@ const TicketControl = require("../models/ticket-control");
 const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
+
+    socket.emit('ultimo-ticket', ticketControl.ultimo );
     
     socket.on('sigiente-ticket', ( payload, callback ) => {
         const siguiente = ticketControl.siguiente();
